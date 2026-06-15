fe138af95eb365ca5db7332686b42eb24e9d60fc
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-16 00:24:46 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,16 @@
+public class operator {
+    public static void main(String[] args) {
+        int ratingpoint =120;
+        ratingpoint +=20;
+        System.out.println(ratingpoint);
+        ratingpoint -= 20;
+        System.out.println(ratingpoint);
+        ratingpoint *=4;
+        System.out.println(ratingpoint);
+        ratingpoint /=2;
+        System.out.println(ratingpoint);
+        ratingpoint %=3;
+        System.out.println(ratingpoint);
+
+    }
+}
