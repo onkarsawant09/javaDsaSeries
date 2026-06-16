9427a4d34ccf66bd2d5cd0790822f458ff6f0d08
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-17 00:58:46 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i*12);
+            i++;
+
+        }while(i<=10);
+    }
+
+}
