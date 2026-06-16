988313c9eb3b6fa293e2d2465a7695e3f66c63fb
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-17 01:00:00 +0530

    Add files via upload

---
+++ whieloo.java
@@ -0,0 +1,16 @@
+public class whieloo {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=5){
+            System.out.println(i);
+            i++;
+        }
+        //using while loop print 7 table
+        int j=1;
+        while(j<=10){
+            System.out.println(7*j);
+            j++;
+        }
+
+    }
+}
