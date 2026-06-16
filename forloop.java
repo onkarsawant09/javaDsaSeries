f125ce11a3cbfdb984688e90b409defc2afa10ae
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-17 00:58:16 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class forloop {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i ++){
+            System.out.println(i);
+        }
+        System.out.println("table of 2");
+        for(int i=1;i<=10;i++){
+            System.out.println("2*"+i+"="+i*2);
+        }
+    }
+}
