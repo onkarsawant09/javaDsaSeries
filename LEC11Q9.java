c0d7d6c26600996f58f60713688940dea8bb721e
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-22 21:33:41 +0530

    Add files via upload

---
+++ LEC11Q9.java
@@ -0,0 +1,27 @@
+public class LEC11Q9 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row=1;row<=n;row++){
+                for(int col=1;col<=n-row ;col++){
+                    System.out.print("  ");
+                }
+                //part 2
+            if(row==1 ||row==n){
+                for (int col=1;col<=2*row-1;col++){
+                    System.out.print("* ");
+                }
+            }
+            else  {
+                //middle row
+                //1*
+                //2*row-3 spaces
+                System.out.print("* ");
+                for (int col=1 ;col<=2*row-3;col++){
+                    System.out.print("  ");
+                }
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
