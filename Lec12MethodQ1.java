700a72d736189f88201f8455d3b0bdeaeb9d0fe7
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-25 23:36:22 +0530

    Add files via upload

---
+++ Lec12MethodQ1.java
@@ -0,0 +1,12 @@
+public class Lec12MethodQ1 {
+   static void twotable(){
+        for (int i=1;i<=10;i++){
+            int ans=2*i;
+            System.out.println(ans);
+        }
+   }
+
+    public static void main(String[] args) {
+        twotable();
+    }
+    }
