5a59a67ab18d69aadd772f50bac38d4959adeaba
buildwithtiger <jg5npdpmcx@privaterelay.appleid.com>
2026-06-25 23:36:44 +0530

    Add files via upload

---
+++ Lec2method2.java
@@ -0,0 +1,10 @@
+public class Lec2method2 {
+    static void printsum(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        printsum(22,44);
+    }
+
+}
